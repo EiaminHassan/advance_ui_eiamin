e90b31ed81833b24033306bdfff4e8df60df6f54
EiaminHassan <shanto15-5251@diu.edu.bd>
2025-02-25 10:49:07 +0600

    cached_image

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,21 +11,26 @@ class Lab06AdvanceUI extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Advance Flutter UI",
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("ADvance UI"),
-        ),
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              Image.network("https://plus.unsplash.com/premium_photo-1683910767532-3a25b821f7ae?q=80&w=2008&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"),
-              Image.network("https://images.unsplash.com/photo-1626808642875-0aa545482dfb?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"),
-
-            ],
+        title: "Advance Flutter UI",
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text("ADvance UI"),
           ),
-        ),
-      )
-    );
+          body: SingleChildScrollView(
+            child: Column(
+              children: [
+                CachedNetworkImage(
+                  imageUrl: "http://via.placeholder.com/350x150",
+                  placeholder: (context, url) => CircularProgressIndicator(),
+                  errorWidget: (context, url, error) => Icon(Icons.error),
+                ),
+                Image.network(
+                    "https://plus.unsplash.com/premium_photo-1683910767532-3a25b821f7ae?q=80&w=2008&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"),
+                Image.network(
+                    "https://images.unsplash.com/photo-1626808642875-0aa545482dfb?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"),
+              ],
+            ),
+          ),
+        ));
   }
 }
